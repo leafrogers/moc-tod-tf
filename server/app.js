e9c24ed26a718e713d2537e7678213bd5e255229
Leaf Rogers <150157+leafrogers@users.noreply.github.com>
2021-06-21 19:46:40 +0100

    feat: add rudimentary ft.com mirroring thingy

--- server/app.js
+++ server/app.js
@@ -1,9 +1,29 @@
 import express from 'express';
 import helmet from 'helmet';
+import fetch from 'node-fetch';
 
 const app = express();
 
 app.use(helmet());
-app.get('/', ({ res }) => res.sendStatus(200));
+
+app.get('/', (_, res, next) => {
+	fetch('https://www.ft.com/?format=html')
+		.then((ftResponse) => {
+			if (ftResponse.ok) {
+				return ftResponse.text();
+			}
+
+			throw new Error(`Nah: ${res.status}`);
+		})
+		.then((html) => {
+			const toSend = (html || '').replace(
+				'overflow-x:hidden;',
+				'overflow-x:hidden;transform:scaleX(-1);'
+			);
+			res.set('Content-Type', 'text/html');
+			res.send(toSend);
+		})
+		.catch(next);
+});
 
 export default app;
